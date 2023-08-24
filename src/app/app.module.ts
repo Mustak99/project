ee1d68e598c3c9598a340019a5d4b4802f5c02ad
Mustak99 <mustakshaikh217@gmail.com>
2023-08-23 18:25:30 -0700

    optimised code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { OnlineMachineComponent } from './online-machine/online-machine.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component'; 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     OnlineMachineComponent,
     HeaderComponent
   ],
